15e2f0fae05e4efe0f9fbe4bb7ff50df8477ca4e
Fabian Ritter <fabian.ritter@cs.uni-saarland.de>
2021-06-11 11:09:18 +0200

    Add some formalities to use this as a package

---
+++ schemes/README.md
@@ -0,0 +1,1 @@
+This directory is for storing the instruction schemes ISAs.
---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import pathlib
+import setuptools
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setuptools.setup(
+    name="iwho",
+    version="0.0.1",
+    description="IWHo: Instructions With Holes",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Fabian Ritter",
+    author_email="fabian.ritter@cs.uni-saarland.de",
+    # license="MIT",
+    # classifiers=[
+    #     "License :: OSI Approved :: MIT License",
+    #     "Programming Language :: Python"
+    # ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3"
+)
