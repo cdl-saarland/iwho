4cf2583753c5a50170227cd11074f37f2f386175
Fabian Ritter <fabian.ritter@cs.uni-saarland.de>
2021-08-17 10:53:34 +0200

    Add an `update_logfile` utility function
    This is necessary as repeatedly calling `logging.basicConfig` does
    nothing after the first call.

--- iwho/utils.py
+++ iwho/utils.py
@@ -4,7 +4,9 @@
 
 from collections import defaultdict
 
+import logging
 import sys
+
 
 def export(fn):
     """ A decorator to automatically add the decorated object to the __all__
@@ -45,7 +47,17 @@ def is_hex_str(hex_str: str) -> bool:
 
 
 def init_logging(loglevel, logfile=None):
-    import logging
+    """ Initialize the python logging facilities.
+
+    `loglevel` should be a string from ["debug", "info", "warning", "error",
+    "critical"], indicating what kind of logging messages to display.
+    Logging is always printed to stderr, and, if it is not None, also to the
+    file at `logfile`.
+
+    Only call this function once per execution, subsequent calls will not do
+    anything. If you want to change the logfile on the fly, use the
+    `update_logfile` function instead.
+    """
     numeric_level = getattr(logging, loglevel.upper(), None)
     if not isinstance(numeric_level, int):
         raise ValueError('Invalid log level: {}'.format(loglevel))
@@ -61,6 +73,29 @@ def init_logging(loglevel, logfile=None):
         }
 
     logging.basicConfig(**kwargs)
+
+def update_logfile(logfile=None):
+    """ Change the logfile used by the logging facilities.
+
+    This will remove any FileHandlers from the root logger and, if it is not
+    None, will add a new handler for the specified `logfile`.
+    StreamHandlers are not touched by this.
+
+    Only use this function after initializing the logging facilities.
+    """
+
+    log = logging.getLogger()  # root logger
+
+    for hdlr in log.handlers[:]:
+        if isinstance(hdlr,logging.FileHandler):
+            log.removeHandler(hdlr)
+
+    if logfile is None:
+        return
+
+    new_filehandler = logging.FileHandler(logfile)
+    log.addHandler(new_filehandler)
+
 
 def parse_args_with_logging(argparser, default_loglevel="warning"):
     loglevels = ["debug", "info", "warning", "error"]
