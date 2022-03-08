9589d6655485e62fb9f9c4152e4bc494b7586c97
Fabian Ritter <fabian.ritter@cs.uni-saarland.de>
2022-03-08 15:23:17 +0100

    Work on setup

--- setup.py
+++ setup.py
@@ -2,9 +2,30 @@
 
 import pathlib
 import setuptools
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+
+import shutil
+import sys
 
 HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
+LICENSE = (HERE / "LICENSE").read_text()
+
+def post_command(a):
+    # TODO extract the insn schemes here
+    pass
+
+class PostDevelopCommand(develop):
+    def run(self):
+        develop.run(self)
+        post_command(self)
+
+class PostInstallCommand(install):
+    def run(self):
+        install.run(self)
+        post_command(self)
+
 
 setuptools.setup(
     name="iwho",
@@ -14,15 +35,16 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     author="Fabian Ritter",
     author_email="fabian.ritter@cs.uni-saarland.de",
-    # license="MIT",
-    # classifiers=[
-    #     "License :: OSI Approved :: MIT License",
-    #     "Programming Language :: Python"
-    # ],
-    packages=setuptools.find_packages(),
-    python_requires=">=3",
+    license=LICENSE,
+    packages=setuptools.find_packages(exclude=('tests',)),
+    cmdclass={
+        'develop': PostDevelopCommand,
+        'install': PostInstallCommand,
+    },
     install_requires=[
         "pyparsing",
         "rpyc",
-    ]
+    ],
+    scripts=["tool/iwho-predict"],
+    python_requires=">=3",
 )
