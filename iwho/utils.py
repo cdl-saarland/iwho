dc3fcdfa2141e1722eb3d651d7547ce142a1e324
Fabian Ritter <fabian.ritter@cs.uni-saarland.de>
2022-06-30 14:27:50 +0200

    Improve iwho.utils documentation

--- iwho/utils.py
+++ iwho/utils.py
@@ -1,4 +1,4 @@
-""" Helper classes and functions for iwho
+""" Helper classes and functions for IWHO
 """
 
 
@@ -10,6 +10,7 @@ import sys
 
 def timestamped_filename(specifier, ending, *, modifier=None, microsecs=False):
     """ Produce a timestamped file name, e.g. for producing logging results.
+
     The specifier should describe what is written to the file, the ending is
     the file ending (without dot), and the optional modifier can be used to add
     more context to the file name after the timestamp.
@@ -25,7 +26,7 @@ def timestamped_filename(specifier, ending, *, modifier=None, microsecs=False):
     return outfilename
 
 def export(fn):
-    """ A decorator to automatically add the decorated object to the __all__
+    """ A decorator to automatically add the decorated object to the `__all__`
     list of the module (which describes what is imported in
     `from module import *` statements).
     """
@@ -36,14 +37,38 @@ def export(fn):
         mod.__all__ = [fn.__name__]
     return fn
 
+
 class DedupStore:
-    """ TODO document thouroughly
+    """ A tool to store and manage a number of hashable objects without
+    duplicates.
+
+    The idea is that instead of constructing an object directly
+    (`obj = A(**kwargs)`), you use the `DedupStore.get()` method instead:
+
+    ```
+        store = DedupStore()
+        #...
+        obj = store.get(A, **kwargs)
+    ```
+
+    If an object of type `A` with equal `kwargs` has been obtained from
+    `store.get()` before, it is returned. Otherwise, the object is created,
+    stored for future `get` calls, and returned. Only keyword arguments are
+    allowed.
     """
     def __init__(self):
         self.stores = defaultdict(dict)
 
     def get(self, constructor, unhashed_kwargs=dict(), **kwargs):
-        """ TODO document
+        """ Obtain a fitting object of the `constructor` class with the
+        provided arguments.
+
+        Take a previously created one if possible; create and register a new
+        one if necessary.
+
+        With `unhashed_kwargs`, additional arguments that should not be hashed
+        for the lookup can be passed to the constructor. This is helpful for,
+        e.g., when created objects need a reference to a global context object.
         """
         store = self.stores[constructor]
         key = tuple(sorted(kwargs.items(), key=lambda x: x[0]))
@@ -55,6 +80,9 @@ class DedupStore:
         return new_res
 
 def is_hex_str(hex_str: str) -> bool:
+    """ Return `True` iff the `hex_str` can be interpreted as a hexadecimal
+    number.
+    """
     try:
         int(hex_str, 16)
     except ValueError as e:
@@ -65,9 +93,9 @@ def is_hex_str(hex_str: str) -> bool:
 def init_logging(loglevel, logfile=None):
     """ Initialize the python logging facilities.
 
-    `loglevel` should be a string from ["debug", "info", "warning", "error",
-    "critical"], indicating what kind of logging messages to display.
-    Logging is always printed to stderr, and, if it is not None, also to the
+    `loglevel` should be a string from `["debug", "info", "warning", "error",
+    "critical"]``, indicating what kind of logging messages to display.
+    Logging is always printed to stderr, and, if it is not `None`, also to the
     file at `logfile`.
 
     Only call this function once per execution, subsequent calls will not do
@@ -93,9 +121,9 @@ def init_logging(loglevel, logfile=None):
 def update_logfile(logfile=None):
     """ Change the logfile used by the logging facilities.
 
-    This will remove any FileHandlers from the root logger and, if it is not
-    None, will add a new handler for the specified `logfile`.
-    StreamHandlers are not touched by this.
+    This will remove any `FileHandler`s from the root logger and, if it is not
+    `None`, will add a new handler for the specified `logfile`.
+    `StreamHandler`s are not touched by this.
 
     Only use this function after initializing the logging facilities.
     """
@@ -114,6 +142,10 @@ def update_logfile(logfile=None):
 
 
 def parse_args_with_logging(argparser, default_loglevel="warning"):
+    """ Convenience wrapper around `argparser.parse_args()` to inject logging
+    arguments and to handle them right after argument parsing to initalize
+    logging.
+    """
     loglevels = ["debug", "info", "warning", "error"]
     argparser.add_argument("-l", "--loglevel", choices=loglevels, default=default_loglevel,
             help="configures the amount of logging information to print")
